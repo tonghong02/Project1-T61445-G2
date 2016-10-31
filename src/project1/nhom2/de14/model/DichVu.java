287825d1249ceea3ca5cfc77989534113147061c
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2016-10-31 20:29:04 +0700

    CHoaDon + CDatPhong

--- src/project1/nhom2/de14/model/DichVu.java
+++ src/project1/nhom2/de14/model/DichVu.java
@@ -10,40 +10,62 @@ package project1.nhom2.de14.model;
  * @author LeThanhLoi
  */
 public class DichVu {
-    private String maDV;
-    private String tenDV;
-    private String donGia;
-    private String maNVPhuTrach;
 
-    public String getMaDV() {
-        return maDV;
-    }
+	private String maDV;
+	private String tenDV;
+	private double donGia;
+	private String maNVPhuTrach;
 
-    public void setMaDV(String maDV) {
-        this.maDV = maDV;
-    }
+	public DichVu(String record){
+		String [] s = new String[4];
+		s = record.split("\t", 4);
+		
+		maDV = s[0];
+		tenDV = s[1];
+		donGia = Double.parseDouble(s[2]);
+		maNVPhuTrach = s[3];
+	}
+	
+	public String getMaDV() {
+		return maDV;
+	}
 
-    public String getTenDV() {
-        return tenDV;
-    }
+	public void setMaDV(String maDV) {
+		this.maDV = maDV;
+	}
 
-    public void setTenDV(String tenDV) {
-        this.tenDV = tenDV;
-    }
+	public String getTenDV() {
+		return tenDV;
+	}
 
-    public String getDonGia() {
-        return donGia;
-    }
+	public void setTenDV(String tenDV) {
+		this.tenDV = tenDV;
+	}
 
-    public void setDonGia(String donGia) {
-        this.donGia = donGia;
-    }
+	public double getDonGia() {
+		return donGia;
+	}
 
-    public String getMaNVPhuTrach() {
-        return maNVPhuTrach;
-    }
+	public void setDonGia(double donGia) {
+		this.donGia = donGia;
+	}
 
-    public void setMaNVPhuTrach(String maNVPhuTrach) {
-        this.maNVPhuTrach = maNVPhuTrach;
-    }
+	public String getMaNVPhuTrach() {
+		return maNVPhuTrach;
+	}
+
+	public void setMaNVPhuTrach(String maNVPhuTrach) {
+		this.maNVPhuTrach = maNVPhuTrach;
+	}
+	
+	public String[] getValues(){
+		String [] values = new String[4];
+		
+		values[0] = maDV;
+		values[1] = tenDV;
+		values[2] = Double.toString(donGia);
+		values[3] = maNVPhuTrach;
+		
+		return values;
+	}
 }
--- src/project1/nhom2/de14/model/HoaDon.java
+++ src/project1/nhom2/de14/model/HoaDon.java
@@ -1,52 +1,80 @@
-
 package project1.nhom2.de14.model;
 
+import java.util.GregorianCalendar;
+import project1.nhom2.de14.util.CalendarAdapter;
 
 public class HoaDon {
-    private String maHD;
-    private String maDatPhong;
-    private String thoiGianThanhToan;
-    private String tienPhong;
-    private String tienDichvu;
 
-    public String getMaHD() {
-        return maHD;
-    }
+	private String maHD;
+	private String maDatPhong;
+	private GregorianCalendar thoiGianThanhToan;
+	private double tienPhong;
+	private double tienDichvu;
+	
+	public HoaDon(String record){
+		String[] s = new String[5];
+		s = record.split("\t",5);
+		
+		maHD = s[0];
+		maDatPhong = s[1];
+		thoiGianThanhToan = new GregorianCalendar();
+		thoiGianThanhToan.setTime(CalendarAdapter.StringToDate(s[2]));
+		tienPhong = Double.parseDouble(s[3]);
+		tienDichvu = Double.parseDouble(s[4]);
+	}
 
-    public void setMaHD(String maHD) {
-        this.maHD = maHD;
-    }
+	public String getMaHD() {
+		return maHD;
+	}
 
-    public String getMaDatHang() {
-        return maDatPhong;
-    }
+	public void setMaHD(String maHD) {
+		this.maHD = maHD;
+	}
 
-    public void setMaDatHang(String maDatPhong) {
-        this.maDatPhong = maDatPhong;
-    }
+	public String getMaDatHang() {
+		return maDatPhong;
+	}
 
-    public String getThoiGianThanhToan() {
-        return thoiGianThanhToan;
-    }
+	public void setMaDatHang(String maDatPhong) {
+		this.maDatPhong = maDatPhong;
+	}
 
-    public void setThoiGianThanhToan(String thoiGianThanhToan) {
-        this.thoiGianThanhToan = thoiGianThanhToan;
-    }
+	public GregorianCalendar getThoiGianThanhToan() {
+		return thoiGianThanhToan;
+	}
+	public String getThoiGianThanhToanString(){
+		return CalendarAdapter.formatCalendar(thoiGianThanhToan);
+	}
+	
+	public void setThoiGianThanhToan(GregorianCalendar thoiGianThanhToan) {
+		this.thoiGianThanhToan = thoiGianThanhToan;
+	}
 
-    public String getTienPhong() {
-        return tienPhong;
-    }
+	public double getTienPhong() {
+		return tienPhong;
+	}
 
-    public void setTienPhong(String tienPhong) {
-        this.tienPhong = tienPhong;
-    }
+	public void setTienPhong(double tienPhong) {
+		this.tienPhong = tienPhong;
+	}
 
-    public String getTienDichvu() {
-        return tienDichvu;
-    }
+	public double getTienDichvu() {
+		return tienDichvu;
+	}
 
-    public void setTienDichvu(String tienDichvu) {
-        this.tienDichvu = tienDichvu;
-    }
-    
+	public void setTienDichvu(double tienDichvu) {
+		this.tienDichvu = tienDichvu;
+	}
+
+	public String[] getValues(){
+		String [] values = new String[5];
+		
+		values[0] = maHD;
+		values[1] = maDatPhong;
+		values[2] = getThoiGianThanhToanString();
+		values[3] = Double.toString(tienPhong);
+		values[4] = Double.toString(tienDichvu);
+		
+		return values;
+	}
 }
